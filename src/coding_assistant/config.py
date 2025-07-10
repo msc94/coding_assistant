1e52f95dffa47005367288bb6deb9e66796a9835
Marcel Schneider <9115235+msc94@users.noreply.github.com>
2025-07-10 20:02:03 +0200

    Revert "Update"
    This reverts commit f80b04a89528d582e5b4c2649d192ca9d477e0f1.

--- src/coding_assistant/config.py
+++ src/coding_assistant/config.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Optional, Callable
-from io import TextIOWrapper
 
 from langchain_core.language_models import BaseChatModel
 
@@ -11,8 +10,6 @@ class Config:
     working_directory: Path = None
     model_factory: Callable[[], BaseChatModel] = None
     reasoning_model_factory: Callable[[], BaseChatModel] = None
-    # A file into which tracing information is written (all agent output, all tool output)
-    tracing_file: Optional[TextIOWrapper] = None
 
 
 config: Config = Config()
--- src/coding_assistant/logging.py
+++ src/coding_assistant/logging.py
@@ -1,4 +1,3 @@
-import json
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.messages import ToolMessage, AIMessage
 from rich.panel import Panel
@@ -9,83 +8,27 @@ import logging
 
 from pprint import pformat
 
-from coding_assistant.config import get_global_config
-
 console = Console()
 logger = logging.getLogger(__name__)
 
 
-def print_to_console(content, do_print):
-    if do_print:
-        console.print(content)
-
-
-def trace_to_file(data, tracing_file):
-    if tracing_file:
-        tracing_file.write(json.dumps(data) + "\n")
-
-
-def print_agent_progress(chunk, name, do_print=False):
-    tracing_file = get_global_config().tracing_file
-
+def print_agent_progress(chunk, name, print_to_console=False):
     if "tools" in chunk and "messages" in chunk["tools"]:
         for message in chunk["tools"]["messages"]:
             assert isinstance(message, ToolMessage)
-            print_to_console(
-                Panel(
-                    message.content,
-                    title=f"Tool ({message.name})",
-                    border_style="yellow",
-                ),
-                do_print,
-            )
-
-            trace_to_file(
-                {
-                    "type": "tool",
-                    "name": message.name,
-                    "content": message.content,
-                },
-                tracing_file,
-            )
+            console.print(Panel(message.content, title=f"Tool ({message.name})", border_style="yellow"))
     elif "agent" in chunk and "messages" in chunk["agent"]:
         for message in chunk["agent"]["messages"]:
             assert isinstance(message, AIMessage)
             title = f"Agent progress: {name}"
             if message.content:
-                print_to_console(
-                    Panel(Markdown(message.content), title=title, border_style="red"),
-                    print_to_console,
-                )
-
-                trace_to_file(
-                    {
-                        "type": "agent_output",
-                        "agent": name,
-                        "content": message.content,
-                    },
-                    tracing_file,
-                )
-
+                console.print(Panel(Markdown(message.content), title=title, border_style="red"))
             if message.tool_calls:
+                content = ""
                 for tool_call in message.tool_calls:
-                    print_to_console(
-                        Panel(
-                            f"{tool_call.get('name')}\n{pformat(tool_call.get('args'))}\n",
-                            title=title,
-                            border_style="red",
-                        ),
-                        print_to_console,
-                    )
-
-                    trace_to_file(
-                        {
-                            "type": "agent_tool_call",
-                            "agent": name,
-                            "tool": tool_call.get("name"),
-                            "content": message.content,
-                        },
-                        tracing_file,
-                    )
+                    pretty_args = pformat(tool_call.get("args"))
+                    content += f"{tool_call.get('name')}\n{pretty_args}\n"
+                content = content.strip()
+                console.print(Panel(content, title=title, border_style="red"))
     else:
         logger.warning(f"Unhandled chunk: {pformat(chunk)}")
