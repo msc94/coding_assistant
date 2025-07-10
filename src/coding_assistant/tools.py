6aef3227bf90155d8d4d868a30b831545d8b305a
Marcel Schneider <9115235+msc94@users.noreply.github.com>
2025-07-10 20:02:08 +0200

    Update get_all_mcp_servers: comment out git and shell server inclusion to reflect recent refactor.

--- src/coding_assistant/tools.py
+++ src/coding_assistant/tools.py
@@ -131,9 +131,9 @@ async def get_all_mcp_servers(config: Config) -> AsyncGenerator[List[MCPServer],
 
     async with AsyncExitStack() as stack:
         servers.append(await stack.enter_async_context(get_filesystem_server(config)))
-        servers.append(await stack.enter_async_context(get_git_server(config)))
+        # servers.append(await stack.enter_async_context(get_git_server(config)))
         servers.append(await stack.enter_async_context(get_fetch_server()))
-        servers.append(await stack.enter_async_context(get_shell_server()))
+        # servers.append(await stack.enter_async_context(get_shell_server()))
 
         if os.environ.get("TAVILY_API_KEY"):
             servers.append(await stack.enter_async_context(get_tavily_server()))
