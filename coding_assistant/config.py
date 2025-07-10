3a6b6a0dc4b8ebaa2c598f9c9e20aa70d698e9fb
Marcel Schneider <9115235+msc94@users.noreply.github.com>
2025-07-10 20:02:03 +0200

    Update

--- coding_assistant/config.py
+++ coding_assistant/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Optional, Callable
+from io import TextIOWrapper
 
 from langchain_core.language_models import BaseChatModel
 
@@ -10,6 +11,8 @@ class Config:
     working_directory: Path = None
     model_factory: Callable[[], BaseChatModel] = None
     reasoning_model_factory: Callable[[], BaseChatModel] = None
+    # A file into which tracing information is written (all agent output, all tool output)
+    tracing_file: Optional[TextIOWrapper] = None
 
 
 config: Config = Config()
--- coding_assistant/logging.py
+++ coding_assistant/logging.py
@@ -1,3 +1,4 @@
+import json
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.messages import ToolMessage, AIMessage
 from rich.panel import Panel
@@ -8,27 +9,79 @@ import logging
 
 from pprint import pformat
 
+from coding_assistant.config import get_global_config
+
 console = Console()
 logger = logging.getLogger(__name__)
 
 
-def print_agent_progress(chunk, name):
+def print_to_console(content, print_to_console):
+    if print_to_console:
+        console.print(content)
+
+
+def trace_to_file(data, tracing_file):
+    if tracing_file:
+        tracing_file.write(json.dumps(data) + "\n")
+
+
+def print_agent_progress(chunk, name, print_to_console=False):
+    tracing_file = get_global_config().tracing_file
+
     if "tools" in chunk and "messages" in chunk["tools"]:
         for message in chunk["tools"]["messages"]:
             assert isinstance(message, ToolMessage)
-            console.print(Panel(message.content, title=f"Tool ({message.name})", border_style="yellow"))
+            print_to_console(
+                Panel(message.content, title=f"Tool ({message.name})", border_style="yellow"),
+                print_to_console,
+            )
+
+            trace_to_file(
+                {
+                    "type": "tool",
+                    "name": message.name,
+                    "content": message.content,
+                },
+                tracing_file,
+            )
     elif "agent" in chunk and "messages" in chunk["agent"]:
         for message in chunk["agent"]["messages"]:
             assert isinstance(message, AIMessage)
             title = f"Agent progress: {name}"
             if message.content:
-                console.print(Panel(Markdown(message.content), title=title, border_style="red"))
+                print_to_console(
+                    Panel(Markdown(message.content), title=title, border_style="red"),
+                    print_to_console,
+                )
+
+                trace_to_file(
+                    {
+                        "type": "agent_output",
+                        "agent": name,
+                        "content": message.content,
+                    },
+                    tracing_file,
+                )
+
             if message.tool_calls:
-                content = ""
                 for tool_call in message.tool_calls:
-                    pretty_args = pformat(tool_call.get("args"))
-                    content += f"{tool_call.get('name')}\n{pretty_args}\n"
-                content = content.strip()
-                console.print(Panel(content, title=title, border_style="red"))
+                    print_to_console(
+                        Panel(
+                            f"{tool_call.get('name')}\n{pformat(tool_call.get('args'))}\n",
+                            title=title,
+                            border_style="red",
+                        ),
+                        print_to_console,
+                    )
+
+                    trace_to_file(
+                        {
+                            "type": "agent_tool_call",
+                            "agent": name,
+                            "tool": tool_call.get("name"),
+                            "content": message.content,
+                        },
+                        tracing_file,
+                    )
     else:
         logger.warning(f"Unhandled chunk: {pformat(chunk)}")
