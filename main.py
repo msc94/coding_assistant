f290c010df0b598e1b9e02d91039b0f7ce97b024
Marcel Schneider <9115235+msc94@users.noreply.github.com>
2025-07-10 20:02:02 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage
+from langchain_google_community import GoogleSearchAPIWrapper, GoogleSearchResults
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+
+# Create the agent
+memory = MemorySaver()
+model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
+wrapper = GoogleSearchAPIWrapper()
+search = GoogleSearchResults(api_wrapper=wrapper)
+tools = [search]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+# Use the agent
+config = {"configurable": {"thread_id": "thread-1"}}
+
+for chunk in agent_executor.stream(
+    {"messages": [HumanMessage(content="hi im bob! and i live in sf")]}, config
+):
+    print(chunk)
+    print("----")
+
+for chunk in agent_executor.stream(
+    {"messages": [HumanMessage(content="whats the weather where I live?")]}, config
+):
+    print(chunk)
+    print("----")
