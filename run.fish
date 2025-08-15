0c7ea63502f2f8d263f7a2c796460b945e68b060
Marcel Schneider <9115235+msc94@users.noreply.github.com>
2025-08-15 23:22:15 +0200

    Update

--- run.fish
+++ run.fish
@@ -5,7 +5,7 @@ uv --project (dirname (status filename)) run coding-assistant \
     --expert-model "gemini/gemini-2.5-pro" \
     --readable-sandbox-directories /mnt/wsl ~/.ssh \
     --writable-sandbox-directories /tmp /dev/shm ~/.serena \
-    --no-truncate-tools "mcp_context7_get-library-docs" \
+    --no-truncate-tools "^mcp_context7_get-library-docs" \
     --instructions "- Prefer the tools from the `serena` MCP server." \
     --mcp-servers \
         '{"name": "serena", "command": "uvx", "args": ["--from", "git+https://github.com/oraios/serena@d5f90710676b6a7cacc450f59005b4934c49b6db", "serena", "start-mcp-server", "--project", "{working_directory}"], "env": []}' \
